cdd7ebb225ea0b495b606db730cb8986acb7d9ab
Nicolas RICHARD <nicolas.richard1@etu.univ-nantes.fr>
2017-03-21 14:35:13 +0100

    init

---
+++ javascript/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $("#champsSaisie").on("keyup",function(){
+    $("#listeCommunes").empty();
+    $.getJSON("http://infoweb-ens/~jacquin-c/codePostal/commune.php?commune="+$("#champsSaisie").val()+"&maxRows=5", function(data){
+        for (var i in data) {
+          $("#listeCommunes").append("<option value='"+data[i].Ville+"'>"+data[i].Ville+"</option>");
+        }
+      });
+  });
+
+
+  $("#soumission").on("click",function(){
+    $("#images").empty();
+    $.ajax({
+    url:'http://api.flickr.com/services/feeds/photos_public.gne',
+    type:'GET',
+    dataType:'jsonp',
+    jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
+    data:'tags='+$("#champsSaisie").val()+'&tagmode=any&format=json',
+    success:function(data){
+    $.each(data.items, function(i,item){
+                $("<img/>").attr("src", item.media.m).appendTo("#images");
+                if ( i == 6 ) return false ; });
+              },
+    error: function(resultat,statut,erreur){
+    alert("erreur");},
+     });
+  });
+
+  $("#slider").slider({
+    max:5,
+    min:0
+  });
+
+
+
+
+});
